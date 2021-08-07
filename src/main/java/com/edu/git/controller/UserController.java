7baffb85cf43245a3a8147bd8a5b53e5111d88b2
XiaZhuXi <88519922+XiaZhuXi@users.noreply.github.com>
2021-08-07 13:53:53 +0800

    Update UserController.java

--- src/main/java/com/edu/git/controller/UserController.java
+++ src/main/java/com/edu/git/controller/UserController.java
@@ -4,5 +4,6 @@ public class UserController {
     public static void main(String[] args) {
         System.out.println("test git");
         System.out.println("by git test");
+         System.out.println("by github test");
     }
 }
